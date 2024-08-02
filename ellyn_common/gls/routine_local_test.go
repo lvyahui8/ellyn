9526e89051f7b77b85726d6a6dfe4f8882ab11c9
lvyahui <lvyahui8@gmail.com>
2024-08-02 22:43:30 +0800

    gls testcase

--- ellyn_common/gls/routine_local_test.go
+++ ellyn_common/gls/routine_local_test.go
@@ -24,6 +24,10 @@ func TestRoutineLocalBasic(t *testing.T) {
 	local.Clear()
 	val, ok = local.Get()
 	require.False(t, ok)
+	local.Set(4)
+	val, ok = local.Get()
+	require.True(t, ok)
+	require.Equal(t, 4, val)
 }
 
 func TestRoutineLocalConcurrent(t *testing.T) {
@@ -51,12 +55,19 @@ func TestRoutineLocalConcurrent(t *testing.T) {
 // go tool pprof -http=":8081" profile.pprof
 func BenchmarkRoutineLocal(b *testing.B) {
 	local := RoutineLocal[int]{}
-	local.Set(1)
+	for i := 0; i < 1000; i++ { // 1000个RoutineLocal，相当于context.Context中放了1000个key。
+		other := RoutineLocal[int]{}
+		other.Set(i)
+	}
 	ctx := context.WithValue(context.Background(), "test", "val")
 	// 当ctx有10多个key时，ctx.Value方法实际是一个链表查找，性能还不如routineLocal
-	ctxWithMoreKeys := ctx
+	ctx10Keys := ctx
 	for i := 0; i < 10; i++ {
-		ctxWithMoreKeys = context.WithValue(ctxWithMoreKeys, i, i)
+		ctx10Keys = context.WithValue(ctx10Keys, i, i)
+	}
+	ctx20Keys := ctx
+	for i := 0; i < 20; i++ {
+		ctx20Keys = context.WithValue(ctx20Keys, i, i)
 	}
 	b.Run("routineLocal", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
@@ -68,9 +79,14 @@ func BenchmarkRoutineLocal(b *testing.B) {
 			_ = ctx.Value("test")
 		}
 	})
-	b.Run("ctxMoreKeysGet", func(b *testing.B) {
+	b.Run("ctx10KeysGet", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			_ = ctxWithMoreKeys.Value(3)
+			_ = ctx10Keys.Value(3)
+		}
+	})
+	b.Run("ctx20KeysGet", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = ctx20Keys.Value(3)
 		}
 	})
 }
