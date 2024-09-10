939bac8ec0b60e9ef1336b5c995d26de6e6a2116
lvyahui <lvyahui8@gmail.com>
2024-09-10 23:24:21 +0800

    注释

--- ellyn_common/goroutine/goid.go
+++ ellyn_common/goroutine/goid.go
@@ -2,6 +2,10 @@ package goroutine
 
 import "runtime"
 
+// GetGoId 获取协程id
+// 依赖先执行拷贝动作，将EllynGetGoid方法写入go源码目录，可以参考脚本
+// - goid_init.sh
+// - goid_init.bat
 func GetGoId() uint64 {
 	return runtime.EllynGetGoid()
 }
