e61b1778d1558e3b1c93ef7ec2627e8ebaec78be
lvyahui <lvyahui8@gmail.com>
2024-09-23 00:10:07 +0800

    update

---
+++ viewer/src/Meta.tsx
@@ -0,0 +1,64 @@
+import { Col, Row, Table } from 'antd';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function Meta() {
+    const [data, setData] = useState(null)
+    const [loading,setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        axios.get('')
+            .then(resp => {
+                setData(resp.data)
+                setLoading(false)
+            })
+            .catch(err => {
+                setError(err.message)
+                setLoading(false)
+            })
+    },[])
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>Error: {error}</div>
+    }
+
+    const dataSource = [
+        {
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号',
+        },
+        {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号',
+        },
+    ];
+
+    const columns = [
+        {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: '年龄',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: '住址',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ];
+    return <Table dataSource={dataSource} columns={columns} />
+}
+
+export default Meta
--- viewer/src/main.tsx
+++ viewer/src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import TrafficGraph from './Graph.tsx'
+import Meta from './Meta.tsx'
+import Menus from './Menus.tsx'
+import { Router, Route } from 'react-router'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TrafficGraph />
+      <Menus/>
+      <TrafficGraph/>
+      <Meta/>
   </StrictMode>,
 )
