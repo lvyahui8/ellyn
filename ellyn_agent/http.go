7e2fd57b69e9f9e2d8ddb4446a5f97738cc5f30d
lvyahui <lvyahui8@gmail.com>
2024-09-22 21:26:03 +0800

    增加测试项目

--- ellyn_agent/http.go
+++ ellyn_agent/http.go
@@ -1,11 +1,24 @@
 package ellyn_agent
 
 import (
+	"embed"
 	"github.com/lvyahui8/ellyn/ellyn_common/asserts"
 	"net/http"
 )
 
+//go:embed sources
+var targetSources embed.FS
+
+func init() {
+	go func() {
+		newServer()
+	}()
+}
+
 func newServer() {
+	http.HandleFunc("/meta", func(writer http.ResponseWriter, request *http.Request) {
+		// 元数据检索配置，配置方法采集，配置mock等
+	})
 	http.HandleFunc("/list", func(writer http.ResponseWriter, request *http.Request) {
 		// 流量列表
 	})
--- ellyn_ast/program_test.go
+++ ellyn_ast/program_test.go
@@ -2,6 +2,7 @@ package ellyn_ast
 
 import (
 	"github.com/lvyahui8/ellyn"
+	"github.com/lvyahui8/ellyn/ellyn_agent"
 	"github.com/lvyahui8/ellyn/ellyn_common/utils"
 	"github.com/lvyahui8/ellyn/ellyn_testing"
 	"github.com/stretchr/testify/require"
@@ -17,6 +18,17 @@ func TestProgram(t *testing.T) {
 	prog.Visit()
 	utils.Go.Build(prog.mainPkg.Dir)
 	prog.rollbackAll()
+}
+
+func TestFileEach(t *testing.T) {
+	defer func() {
+		err := recover()
+		require.Nil(t, err)
+	}()
+	prog := NewProgram(ellyn_testing.GetTestProjPath())
+	prog.scanSourceFiles(func(pkg *ellyn_agent.Package, fileAbsPath string) {
+		t.Log(fileAbsPath)
+	})
 }
 
 func TestExample(t *testing.T) {
