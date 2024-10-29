99ad0385b367780b77a37c496eee7ab1f7f17b90
lvyahui <lvyahui8@gmail.com>
2024-10-29 23:16:18 +0800

    增加测试case

--- benchmark/io_scene.go
+++ benchmark/io_scene.go
@@ -1,8 +1,12 @@
 package benchmark
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
 
 var devNull *os.File
+var tmpFile *os.File
 
 func init() {
 	f, err := os.Open(os.DevNull)
@@ -10,8 +14,17 @@ func init() {
 		panic(err)
 	}
 	devNull = f
+	f, err = os.OpenFile(filepath.Join(os.TempDir(), "bench.log"), os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	tmpFile = f
 }
 
 func Write2DevNull(content string) {
 	_, _ = devNull.Write([]byte(content))
+}
+
+func Write2TmpFile(content string) {
+	_, _ = tmpFile.Write([]byte(content))
 }
--- benchmark/io_scene_test.go
+++ benchmark/io_scene_test.go
@@ -7,3 +7,9 @@ func BenchmarkWrite2DevNull(b *testing.B) {
 		Write2DevNull("hello world")
 	}
 }
+
+func BenchmarkWrite2TmpFile(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Write2TmpFile("hello world")
+	}
+}
