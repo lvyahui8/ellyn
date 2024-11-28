a6e15593da3795173f2a67d26b45a194703b596b
lvyahui <lvyahui8@gmail.com>
2024-11-28 09:50:27 +0800

    增加注释

--- sdk/agent/node.go
+++ sdk/agent/node.go
@@ -2,6 +2,7 @@ package agent
 
 import "sync"
 
+// nodePool 函数节点缓存
 var nodePool = &sync.Pool{
 	New: func() any {
 		return newNode()
@@ -12,15 +13,23 @@ func newNode() *node {
 	return &node{}
 }
 
+// node 代表一个函数节点，一个函数如果被调用多次（循环、递归等），在graph中也只会有一个节点
 type node struct {
+	// recursion 函数是否存在递归调用，这里包括直接递归或者间接递归
 	recursion bool
-	methodId  uint32
-	cost      int64
-	blocks    []bool
-	args      *[]any
-	results   *[]any
+	// methodId 函数id
+	methodId uint32
+	// cost 函数执行耗时, 如果函数执行多次，cost是累加值
+	cost int64
+	// blocks 函数块的覆盖情况
+	blocks []bool
+	// args 函数参数列表
+	args *[]any
+	// results 函数返回值列表
+	results *[]any
 }
 
+// Recycle 回收重置函数节点
 func (n *node) Recycle() {
 	n.recursion = false
 	n.cost = 0
--- sdk/agent/param.go
+++ sdk/agent/param.go
@@ -7,14 +7,18 @@ import (
 	"github.com/lvyahui8/ellyn/sdk/common/utils"
 )
 
+// NotCollected 用来代表不收集的参数
 var NotCollected = &struct {
 	_ byte
 }{}
 
+// NotCollectedDisplay 不收集参数的展示值
 var NotCollectedDisplay = utils.Marshal("[NotCollected]")
 
+// MarshalFailed 序列化失败时的展示值
 var MarshalFailed = utils.Marshal("[Marshal failed]")
 
+// EncodeVars 编码参数列表，入参和出参都使用此方法
 func EncodeVars(vars []any) *[]any {
 	var res []any
 	for _, item := range vars {
@@ -24,7 +28,7 @@ func EncodeVars(vars []any) *[]any {
 		}
 		val := item
 		switch v := item.(type) {
-		case context.Context:
+		case context.Context: // golang context 也支持序列化
 			entries := gocontext.GetContextKeyValues(v)
 			val = utils.GetCodableMap(entries)
 		case *context.Context:
